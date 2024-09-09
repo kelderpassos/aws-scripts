389a5828abbac81f26fed8f943622ac07f524822
Kelder Passos <kelder.passos@gmail.com>
2024-09-09 14:13:05 -0300

    feat: executa método para criar segredos na conta de destino

--- secrets-migration/secrets_migration/main.py
+++ secrets-migration/secrets_migration/main.py
@@ -1,26 +1,17 @@
-import os
-
 from secrets_manager.Secrets_Manager import SecretsManager
-from dotenv import load_dotenv
-
-load_dotenv()
-
-region = os.getenv('REGION')
-env_variables: dict = {
-    'accessKeyId': os.getenv('ACCESS_KEY_ID'),
-    'secretAccessKey': os.getenv('SECRET_ACCESS_KEY'),
-    'sessionToken': os.getenv('AWS_SESSION_TOKEN')
-}
+from constants import ORIGIN_CREDENTIALS, DESTINATION_CREDENTIALS, OLD_ACCOUNT_PREFIX, NEW_ACCOUNT_PREFIX
 
 def execute():
-    secrets_manager = SecretsManager(env_variables, region)
-    secret_list = secrets_manager.list_secrets()
+    original_secrets_manager = SecretsManager(ORIGIN_CREDENTIALS)
+    destination_secrets_manager = SecretsManager(DESTINATION_CREDENTIALS)
+
+    secret_list = original_secrets_manager.list_secrets()
     
     for secret in secret_list:
-        print('secret', secret)
-        secrets_manager.get_secret_value(secret['Name'])
+        value = original_secrets_manager.get_secret_value(secret['Name'], NEW_ACCOUNT_PREFIX, OLD_ACCOUNT_PREFIX)
+        destination_secrets_manager.create_secret(value)
 
-    
+
 
 if __name__ == '__main__':
     execute()
