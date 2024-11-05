c9f3bc5ff302d5638566a80004f91b0f380e1bbd
Kelder Passos <kelder.passos@gmail.com>
2024-11-05 16:02:01 -0300

    feat: adiciona verificação para segredos existentes antes de criar novos na migração

--- secrets-migration/secrets_migration/main.py
+++ secrets-migration/secrets_migration/main.py
@@ -8,10 +8,13 @@ def execute():
     secret_list = original_secrets_manager.list_secrets()
     
     for secret in secret_list:
+        new_secret_name = secret['Name'].replace(OLD_ACCOUNT_PREFIX, NEW_ACCOUNT_PREFIX)
+        if destination_secrets_manager.check_existing_secret(new_secret_name):
+            print(f"Secret {new_secret_name} already exists in the destination account.")
+            continue
+        
         value = original_secrets_manager.get_secret_value(secret['Name'], OLD_ACCOUNT_PREFIX, NEW_ACCOUNT_PREFIX)
         destination_secrets_manager.create_secret(value)
-
-
 
 if __name__ == '__main__':
     execute()
