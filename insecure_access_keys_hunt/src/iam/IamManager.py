d587a5efef3b3389da83f0bd9b44fb72159a19b7
kelderpassos <kelder.passos@gmail.com>
2024-12-20 23:43:22 -0300

    feat: adiciona anotações de tipo para os métodos list_users e list_access_keys na classe IamManager

--- insecure_access_keys_hunt/src/iam/IamManager.py
+++ insecure_access_keys_hunt/src/iam/IamManager.py
@@ -1,18 +1,19 @@
 import boto3
+from typing import List, Dict
+
 
 
 class IamManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self.client = boto3.client("iam")
 
-    def list_users(self) -> list:
+    def list_users(self) -> List[Dict[str, str]]:
         return self.client.list_users()["Users"]
 
-    def list_access_keys(self, users: list):
+    def list_access_keys(self, users: List[Dict[str, str]]) -> List[List[Dict[str, str]]]:
         all_access_key = []
 
         for user in users:
             access_key = self.client.list_access_keys(UserName=user["UserName"])
             all_access_key.append(access_key["AccessKeyMetadata"])
-
         return all_access_key
