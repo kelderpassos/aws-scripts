6a3508f65979eb71ad03d744ec79788708fdd818
kelderpassos <kelder.passos@gmail.com>
2024-12-20 23:43:33 -0300

    feat: adiciona função para gerar relatórios de chaves de acesso e aprimora cálculo de idade das chaves

--- insecure_access_keys_hunt/src/utils/utils.py
+++ insecure_access_keys_hunt/src/utils/utils.py
@@ -1,5 +1,19 @@
 import datetime
+import csv
 
-def get_age(date):
-    age = datetime.datetime.now(datetime.timezone.utc) - date['CreateDate']
+from typing import Dict, Union
+
+def get_age(key_metadata):
+    age = datetime.datetime.now(datetime.timezone.utc) - key_metadata['CreateDate']
     return age.days
+
+
+# : Dict[str, Union[str | datetime.datetime]]
+def generate_report(keys, filename):
+    with open(filename, mode="w", newline="") as file:
+        writer = csv.writer(file)
+        print(writer, writer)
+        writer.writerow(["UserName", "AccessKeyId", "CreateDate", "AgeDays"])
+
+        for key in keys:
+            writer.writerow([key["username"], key["key_id"], key["status"], key["key_age"]])
