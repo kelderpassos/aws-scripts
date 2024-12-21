d1d22aada9c961ce50831bfe550e371f33d4e25c
kelderpassos <kelder.passos@gmail.com>
2024-12-21 19:53:57 -0300

    feat: ajusta inicialização do IamManager para suportar perfis e atualiza chamadas de cliente IAM

--- insecure_access_keys_hunt/src/iam/IamManager.py
+++ insecure_access_keys_hunt/src/iam/IamManager.py
@@ -2,18 +2,20 @@ import boto3
 from typing import List, Dict
 
 
-
 class IamManager:
-    def __init__(self) -> None:
-        self.client = boto3.client("iam")
+    def __init__(self, profile=None) -> None:
+        profile = boto3.session.Session(profile_name=profile)
+        self.iam = profile.client("iam")
 
     def list_users(self) -> List[Dict[str, str]]:
-        return self.client.list_users()["Users"]
+        return self.iam.list_users()["Users"]
 
-    def list_access_keys(self, users: List[Dict[str, str]]) -> List[List[Dict[str, str]]]:
+    def list_access_keys(
+        self, users: List[Dict[str, str]]
+    ) -> List[List[Dict[str, str]]]:
         all_access_key = []
 
         for user in users:
-            access_key = self.client.list_access_keys(UserName=user["UserName"])
+            access_key = self.iam.list_access_keys(UserName=user["UserName"])
             all_access_key.append(access_key["AccessKeyMetadata"])
         return all_access_key
