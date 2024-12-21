560dcc3b612a775e010fa553ca4b0198d2408215
kelderpassos <kelder.passos@gmail.com>
2024-12-21 19:54:06 -0300

    feat: adiciona função para entrada de valores de perfil e limite de idade das chaves no relatório

--- insecure_access_keys_hunt/src/utils/utils.py
+++ insecure_access_keys_hunt/src/utils/utils.py
@@ -1,19 +1,47 @@
-import datetime
 import csv
+import datetime
+import os
+import configparser
 
 from typing import Dict, Union
 
+
 def get_age(key_metadata):
-    age = datetime.datetime.now(datetime.timezone.utc) - key_metadata['CreateDate']
+    age = datetime.datetime.now(datetime.timezone.utc) - key_metadata["CreateDate"]
     return age.days
 
 
-# : Dict[str, Union[str | datetime.datetime]]
-def generate_report(keys, filename):
+def generate_report(keys, filename) -> None:
     with open(filename, mode="w", newline="") as file:
         writer = csv.writer(file)
         print(writer, writer)
         writer.writerow(["UserName", "AccessKeyId", "CreateDate", "AgeDays"])
 
         for key in keys:
-            writer.writerow([key["username"], key["key_id"], key["status"], key["key_age"]])
+            writer.writerow(
+                [key["username"], key["key_id"], key["status"], key["key_age"]]
+            )
+
+
+def input_values() -> Dict[str, Union[str, int]]:
+    path = os.path.expanduser("~/.aws/credentials")
+    config = configparser.ConfigParser()
+    config.read(path)
+    profiles = config.sections()
+
+    print("Perfis disponíveis")
+    for index, profile in enumerate(profiles):
+        print(f"{index + 1} {profile}")
+
+    profile_chosen = int(input("Escolha o perfil pelo número: ")) - 1
+    age_limit = int(input("Defina o limite de idade das chaves: "))
+
+    if not (0 <= profile_chosen < len(profiles)):
+        raise ValueError("Índice inválido")
+
+    mapped_inputs: dict = {
+        "age": age_limit,
+        "profile": profiles[profile_chosen]
+    }
+    return mapped_inputs
+
