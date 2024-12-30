8517e63948219f5fb0f518b7f20b186609d8a0b6
kelderpassos <kelder.passos@gmail.com>
2024-12-29 22:26:06 -0300

    refatora: renomeia e reorganiza funções para melhorar a legibilidade e a estrutura do código

--- insecure_access_keys_hunt/src/utils/utils.py
+++ insecure_access_keys_hunt/src/utils/utils.py
@@ -10,32 +10,12 @@ from dataclasses import dataclass
 class InputValues:
     age: int
     profile: str | None
-    to_delete: int = 2
 
 def get_age(key_metadata):
     age = datetime.datetime.now(datetime.timezone.utc) - key_metadata["CreateDate"]
     return age.days
 
-
-def generate_report(keys, filename) -> None:
-    try:
-        current_directory = os.getcwd()
-        with open(filename, mode="w", newline="") as file:
-            writer = csv.writer(file)
-            writer.writerow(["UserName", "AccessKeyId", "CreateDate", "AgeDays"])
-
-            for key in keys:
-                writer.writerow(
-                    [key["username"], key["key_id"], key["status"], key["key_age"]]
-                )
-        print(f"Relatório {filename} gerado com sucesso no caminho {current_directory}")
-    except PermissionError as e:
-        logging.error(f"Erro de permissão ao escrever o arquivo {filename}: {e}")
-    except Exception as e:
-        logging.error(f"Erro inesperado ao gerar o relatório: {e}")
-
-
-def input_values(delete=2) -> InputValues:
+def access_key_scan() -> InputValues:
     try:
         path = os.path.expanduser("~/.aws/credentials")
 
@@ -58,22 +38,36 @@ def input_values(delete=2) -> InputValues:
             exit(1)
 
         age_limit = int(input("Defina o limite de idade das chaves em dias: "))
-
-        # TODO terminar esta parte
-        print(
-            "Gostaria de apagar as chaves encontradas? Esta ação não pode ser desfeita"
-        )
-        print("1 - sim")
-        print("2 - não")
-        to_delete = int(input())
-
-        if to_delete not in [1, 2]:
-            logging.error("Opção inválida")
     
-        return InputValues(age= age_limit, profile= profiles[profile_chosen], to_delete=to_delete)
+        return InputValues(age=age_limit, profile=profiles[profile_chosen])
     except ValueError as e:
         logging.error(f"Erro de entrada do usuário: {e}")
         exit(1)
     except Exception as e:
         logging.error(f"Erro inesperado: {e}")
         exit(1)
+
+def generate_report(keys, filename) -> None:
+    try:
+        current_directory = os.getcwd()
+        with open(filename, mode="w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(["UserName", "AccessKeyId", "CreateDate", "AgeDays"])
+
+            for key in keys:
+                writer.writerow(
+                    [key["username"], key["key_id"], key["status"], key["key_age"]]
+                )
+        print(f"Relatório {filename} gerado com sucesso no caminho {current_directory}")
+    except PermissionError as e:
+        logging.error(f"Erro de permissão ao escrever o arquivo {filename}: {e}")
+    except Exception as e:
+        logging.error(f"Erro inesperado ao gerar o relatório: {e}")
+
+def delete_keys():
+    print("---------------------------")
+    print('Deseja apagar as chaves encontradas? Esta ação não pode ser desfeita')
+    print('1 - sim')
+    print('2 - não')
+
+    return int(input())
