b0e0cb0c9469ea243820d7da0ee0bd1523fa737e
kelderpassos <kelder.passos@gmail.com>
2025-06-04 15:31:30 -0300

    feat: creates main function to apply logic to SecretsManager methods

---
+++ secrets-migration/main.py
@@ -0,0 +1,10 @@
+def main():
+    """
+    Main function to manage secrets from AWS Secrets Manager.
+    Add your logic here to interact with the SecretsManager class.
+    (e.g., listing secrets, creating secrets, migrating secrets from accounts, etc.)
+    """
+
+
+if __name__ == "__main__":
+    main()
